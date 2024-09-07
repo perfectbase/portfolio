7a0721cf5645cd36bb05ed67520a6e40528bb60b
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-09-07 19:27:29 +0900

    improve mobile design

--- src/app/(homepage)/_components/section.tsx
+++ src/app/(homepage)/_components/section.tsx
@@ -14,7 +14,7 @@ export function Section({
   return (
     <section
       id={id}
-      className={cn("w-full py-12 md:py-24 lg:py-32", className)}
+      className={cn("w-full py-16 md:py-24 lg:py-32", className)}
     >
       <div className="container mx-auto px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-8">
